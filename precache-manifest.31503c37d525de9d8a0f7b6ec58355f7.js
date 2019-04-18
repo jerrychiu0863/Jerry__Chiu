c987f5e6a0998f0923b1a63068fb56356a9310e5
Jerry <m08632000@hotmail.com>
2019-04-18 14:19:51 -0700

    Updates

---
+++ precache-manifest.31503c37d525de9d8a0f7b6ec58355f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f34b836efb84c4abaecf6334cf3253",
+    "url": "/Jerry__Chiu/static/media/winkel.72f34b83.jpg"
+  },
+  {
+    "revision": "9625eb47610dbde997724cd04edb1348",
+    "url": "/Jerry__Chiu/static/media/transitionBGgenerator.9625eb47.jpg"
+  },
+  {
+    "revision": "a6af6e32b49851533584",
+    "url": "/Jerry__Chiu/static/js/runtime~main.45f63a7c.js"
+  },
+  {
+    "revision": "3511736a60f957a81247",
+    "url": "/Jerry__Chiu/static/js/main.bfcc62bd.chunk.js"
+  },
+  {
+    "revision": "1bda1d619f365f886a3c",
+    "url": "/Jerry__Chiu/static/js/2.6c44985e.chunk.js"
+  },
+  {
+    "revision": "3511736a60f957a81247",
+    "url": "/Jerry__Chiu/static/css/main.68f15c20.chunk.css"
+  },
+  {
+    "revision": "ce6cbf27d246f6cc38003e3b1228b960",
+    "url": "/Jerry__Chiu/index.html"
+  }
+];
