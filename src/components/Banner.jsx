a0823c64162a93eed2c260162154922118b3b182
Jerry <m08632000@hotmail.com>
2019-06-07 13:40:00 -0700

    fix animation delay

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,43 +5,66 @@ class Banner extends Component {
   
   componentDidMount() {
     
-    window.addEventListener('load', () => {
+    /*window.addEventListener('DOMContentLoaded', () => {
+      this.removeStopClass();
       setTimeout(() => {
-        this.setAnimation()
+        this.setAnimation();
         setTimeout(() => {
           this.hideStripes();
         }, 800);
       }, 1200);
+    });*/
+    
+    window.addEventListener('DOMContentLoaded', () => {
+      this.removeStopClass();
+      setTimeout(() => {
+        this.hideStripes();
+        this.setAnimation();
+      }, 2000);
     });
     
   }
   
   setAnimation = () => {
-    
     const mainBanner = document.getElementsByClassName('Banner__header--main');
     const subBanner = document.getElementsByClassName('Banner__header--sub');
     mainBanner[0].classList.add('show');
     subBanner[0].classList.add('show');
-    
   }
   
   hideStripes = () => {
-    
     const stripes = document.querySelectorAll('.stripe');
-    [].forEach.call(stripes, stripe => {
-      stripe.classList.add('hide')
-    });
+    //Loop same els to add new class
+    /*[].forEach.call(stripes, stripe => {
+      stripe.classList.add('hide');
+      console.log(stripe);
+    });*/
     
+    /*[...stripes].forEach(stripe => {
+       stripe.classList.add('hide');
+    })*/
+    
+    for(const stripe of stripes) {
+        stripe.classList.add('hide');
+    };
+  }
+  
+  removeStopClass = () => {
+    const stripes = document.querySelectorAll('.stripe');
+    for(const stripe of stripes) {
+      stripe.classList.remove('stop');
+    };
   }
   
   render() {
+    //console.log(document.readyState);
     return (
       <div className="Banner">
         <div>
-          <div className="stripe stripe-0"></div>
-          <div className="stripe stripe-1"></div>
-          <div className="stripe stripe-2"></div>
-          <div className="stripe stripe-3"></div>
+          <div className="stripe stripe-0 stop"></div>
+          <div className="stripe stripe-1 stop"></div>
+          <div className="stripe stripe-2 stop"></div>
+          <div className="stripe stripe-3 stop"></div>
         </div>
         
         <div className="Banner__container">
