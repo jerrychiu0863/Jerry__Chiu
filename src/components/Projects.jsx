b290afa090c30f3ebcbd6f48790fd961b4505530
Jerry Chiu <m08632000@hotmail.com>
2021-11-26 15:58:39 +0800

    update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import '../css/Projects.css';
 
 //Project images
-import winkel from '../assets/projects/winkel_600x299.jpg';
+import onePage from '../assets/projects/one_page_600x299.jpg';
+import clueEvent from '../assets/projects/clue_event_600x299.jpg';
+import clueArticle from '../assets/projects/clue_article_600x299.jpg';
 import cssBGgenerator from '../assets/projects/css-generator_600x299.jpg';
 import DecoMe from '../assets/projects/decome_600x299.jpg';
-import blog from '../assets/projects/blog_600x299.jpg';
-import news from '../assets/projects/news_600x299.jpg';
 import todo from '../assets/projects/todo_600x299.jpg';
 
 //Components
@@ -19,30 +19,34 @@ const Projects = () => {
       <div className="container">
         <HeaderDisplay header={'Projects'} />
         <ProjectItem
-          porjectOne={winkel}
-          titleOne={'Win-Kel'}
-          urlOne={'https://www.win-kel.com/'}
+          porjectOne={onePage}
+          titleOne={'One Page Advertisement'}
+          urlOne={'https://www.clue.com.tw/webinars/LandingPage/Get/Wyf0T'}
+          projectTwo={clueEvent}
+          urlTwo={
+            'https://www.clue.com.tw/webinars/Content/eDM/clue_event.html'
+          }
+          titleTwo={'Event Page'}
+        />
+        <ProjectItem
+          porjectOne={clueArticle}
+          titleOne={'Blog Page'}
+          urlOne={
+            'https://www.clue.com.tw/webinars/Content/eDM/clue_article.html'
+          }
           projectTwo={DecoMe}
           urlTwo={'https://jerrychiu0863.github.io/ecommerce/#/'}
           titleTwo={'DecoMe'}
         />
         <ProjectItem
-          porjectOne={cssBGgenerator}
-          titleOne={'Css Background Generator'}
-          urlOne={
-            'https://jerrychiu0863.github.io/react_redux_transitionBackgroundGenerator/'
-          }
-          projectTwo={blog}
-          titleTwo={'Blog Engine'}
-          urlTwo={'https://stark-brook-84904.herokuapp.com/'}
-        />
-        <ProjectItem
           porjectOne={todo}
           titleOne={'The Eisenhower Matrix Todo App'}
           urlOne={'https://jerrychiu0863.github.io/react_todo_app/'}
-          projectTwo={news}
-          titleTwo={'News Search Engine'}
-          urlTwo={'https://jerrychiu0863.github.io/react_news_app/'}
+          projectTwo={cssBGgenerator}
+          titleTwo={'Css Background Generator'}
+          urlTwo={
+            'https://jerrychiu0863.github.io/react_redux_transitionBackgroundGenerator/'
+          }
         />
       </div>
     </div>
