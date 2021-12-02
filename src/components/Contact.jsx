db2ba713d7410d58f5cb43cdd2a160899edd4f25
Jerry Chiu <m08632000@hotmail.com>
2021-12-02 22:25:59 +0800

    change mail tag

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,24 +4,24 @@ import '../css/Contact.css';
 const contactLists = [
   {
     icon: 'fas fa-map-pin',
-    body: 'Changhua, Taiwan'
+    body: 'Changhua, Taiwan',
   },
   {
     icon: 'fas fa-phone',
-    body: '0910-200-918'
+    body: '0910-200-918',
   },
-  {
-    icon: 'fas fa-envelope',
-    body: 'm08632000@hotmail.com'
-  },
+  // {
+  //   icon: 'fas fa-envelope',
+  //   body: 'm08632000@hotmail.com',
+  // },
   {
     icon: 'fab fa-github',
-    body: 'https://github.com/jerrychiu0863'
-  }
+    body: 'https://github.com/jerrychiu0863',
+  },
 ];
 
 const Contact = () => {
-  const renderContactInfo = lists => {
+  const renderContactInfo = (lists) => {
     return lists.map((list, index) => (
       <li className="Contact__list--item" key={index}>
         <i className={`Contact__list--icon ${list.icon}`} />
@@ -36,7 +36,18 @@ const Contact = () => {
         <div className="Contact__left col-0 col-sm-0 col-md-6" />
         <div className="Contact__right col-12 col-sm-12 col-md-6 container">
           <span className="Contact__right--header">Contact Info</span>
-          <ul className="Contact__list">{renderContactInfo(contactLists)}</ul>
+          <ul className="Contact__list">
+            {renderContactInfo(contactLists)}{' '}
+            <li className="Contact__list--item">
+              <i className="Contact__list--icon fas fa-envelope" />
+              <a
+                className="Contact__list--info"
+                href="mailto:m08632000@hotmail.com"
+              >
+                m08632000@hotmail.com
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
