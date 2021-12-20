b29ae4ae006bb7f6616eb691af403f424b9eb040
Jerry Chiu <m08632000@hotmail.com>
2021-12-20 13:20:52 +0800

    add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import '../css/Projects.css';
 
 //Project images
-import onePage from '../assets/projects/one_page_600x299.jpg';
-import clueEvent from '../assets/projects/clue_event_600x299.jpg';
-import clueArticle from '../assets/projects/clue_article_600x299.jpg';
+// import onePage from '../assets/projects/one_page_600x299.jpg';
+// import clueEvent from '../assets/projects/clue_event_600x299.jpg';
+// import clueArticle from '../assets/projects/clue_article_600x299.jpg';
 import cssBGgenerator from '../assets/projects/css-generator_600x299.jpg';
 import DecoMe from '../assets/projects/decome_600x299.jpg';
 import todo from '../assets/projects/todo_600x299.jpg';
+import Competnion from '../assets/projects/competnion_600x299.jpg';
 
 //Components
 import ProjectItem from './ProjectItem';
@@ -19,16 +20,14 @@ const Projects = () => {
       <div className="container">
         <HeaderDisplay header={'Projects'} />
         <ProjectItem
-          porjectOne={onePage}
-          titleOne={'One Page Advertisement'}
-          urlOne={'https://www.clue.com.tw/webinars/LandingPage/Get/Wyf0T'}
-          projectTwo={clueEvent}
-          urlTwo={
-            'https://www.clue.com.tw/webinars/Content/eDM/clue_event.html'
-          }
-          titleTwo={'Event Page'}
+          porjectOne={Competnion}
+          titleOne={'Animal Adoption Website'}
+          urlOne={'https://competnion.netlify.app/'}
+          projectTwo={DecoMe}
+          urlTwo={'https://jerrychiu0863.github.io/ecommerce/#/'}
+          titleTwo={'DecoMe'}
         />
-        <ProjectItem
+        {/* <ProjectItem
           porjectOne={clueArticle}
           titleOne={'Blog Page'}
           urlOne={
@@ -37,10 +36,10 @@ const Projects = () => {
           projectTwo={DecoMe}
           urlTwo={'https://jerrychiu0863.github.io/ecommerce/#/'}
           titleTwo={'DecoMe'}
-        />
+        /> */}
         <ProjectItem
           porjectOne={todo}
-          titleOne={'The Eisenhower Matrix Todo App'}
+          titleOne={'Todo App'}
           urlOne={'https://jerrychiu0863.github.io/react_todo_app/'}
           projectTwo={cssBGgenerator}
           titleTwo={'Css Background Generator'}
