29ef1253684efbabf26dd587ac2f843d8e38c718
Jerry Chiu <m08632000@hotmail.com>
2021-12-21 22:52:20 +0800

    add projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import '../css/Projects.css';
 
 //Project images
-// import onePage from '../assets/projects/one_page_600x299.jpg';
-// import clueEvent from '../assets/projects/clue_event_600x299.jpg';
-// import clueArticle from '../assets/projects/clue_article_600x299.jpg';
+import onePage from '../assets/projects/one_page_600x299.jpg';
+import clueEvent from '../assets/projects/clue_event_600x299.jpg';
+import clueArticle from '../assets/projects/clue_article_600x299.jpg';
 import cssBGgenerator from '../assets/projects/css-generator_600x299.jpg';
 import DecoMe from '../assets/projects/decome_600x299.jpg';
 import todo from '../assets/projects/todo_600x299.jpg';
@@ -27,20 +27,22 @@ const Projects = () => {
           urlTwo={'https://jerrychiu0863.github.io/ecommerce/#/'}
           titleTwo={'DecoMe'}
         />
-        {/* <ProjectItem
+        <ProjectItem
           porjectOne={clueArticle}
           titleOne={'Blog Page'}
           urlOne={
             'https://www.clue.com.tw/webinars/Content/eDM/clue_article.html'
           }
-          projectTwo={DecoMe}
-          urlTwo={'https://jerrychiu0863.github.io/ecommerce/#/'}
-          titleTwo={'DecoMe'}
-        /> */}
+          projectTwo={clueEvent}
+          urlTwo={
+            'https://www.clue.com.tw/webinars/Content/eDM/clue_event.html'
+          }
+          titleTwo={'Event Page'}
+        />
         <ProjectItem
-          porjectOne={todo}
-          titleOne={'Todo App'}
-          urlOne={'https://jerrychiu0863.github.io/react_todo_app/'}
+          porjectOne={onePage}
+          titleOne={'One Page Adverstisement'}
+          urlOne={'https://www.clue.com.tw/webinars/LandingPage/Get/Wyf0T'}
           projectTwo={cssBGgenerator}
           titleTwo={'Css Background Generator'}
           urlTwo={
