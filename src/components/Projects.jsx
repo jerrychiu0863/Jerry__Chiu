5e2ab3935420d441e3bf8fbe23530182188a16d8
Jerry Chiu <m08632000@hotmail.com>
2022-01-05 16:36:06 +0800

    add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,7 @@ import cssBGgenerator from '../assets/projects/css-generator_600x299.jpg';
 import DecoMe from '../assets/projects/decome_600x299.jpg';
 import Competnion from '../assets/projects/competnion_600x299.jpg';
 import Red from '../assets/projects/red_600x299.jpg';
+import Green from '../assets/projects/green_600x299.jpg';
 
 //Components
 import ProjectItem from './ProjectItem';
@@ -21,31 +22,35 @@ const Projects = () => {
         <HeaderDisplay header={'Projects'} />
         <ProjectItem
           porjectOne={Competnion}
-          titleOne={'Animal Adoption Website'}
-          urlOne={'https://competnion.netlify.app/'}
+          titleOne="Animal Adoption Website"
+          urlOne="https://competnion.netlify.app/"
           projectTwo={DecoMe}
-          urlTwo={'https://jerrychiu0863.github.io/ecommerce/#/'}
-          titleTwo={'DecoMe'}
+          urlTwo="https://jerrychiu0863.github.io/ecommerce/#/"
+          titleTwo="DecoMe"
         />
         <ProjectItem
-          porjectOne={Red}
-          titleOne={'Red'}
-          urlOne={'https://jerrychiu0863.github.io/red/'}
+          porjectOne={clueArticle}
+          titleOne="Blog Page"
+          urlOne="https://www.clue.com.tw/webinars/Content/eDM/clue_article.html"
           projectTwo={clueEvent}
-          urlTwo={
-            'https://www.clue.com.tw/webinars/Content/eDM/clue_event.html'
-          }
-          titleTwo={'Event Page'}
+          urlTwo="https://www.clue.com.tw/webinars/Content/eDM/clue_event.html"
+          titleTwo="Event Page"
         />
         <ProjectItem
           porjectOne={onePage}
-          titleOne={'One Page Adverstisement'}
-          urlOne={'https://www.clue.com.tw/webinars/LandingPage/Get/Wyf0T'}
+          titleOne="One Page Adverstisement"
+          urlOne="https://www.clue.com.tw/webinars/LandingPage/Get/Wyf0T"
           projectTwo={cssBGgenerator}
-          titleTwo={'Css Background Generator'}
-          urlTwo={
-            'https://jerrychiu0863.github.io/react_redux_transitionBackgroundGenerator/'
-          }
+          titleTwo="Css Background Generator"
+          urlTwo="https://jerrychiu0863.github.io/react_redux_transitionBackgroundGenerator/"
+        />
+        <ProjectItem
+          porjectOne={Green}
+          titleOne="Green"
+          urlOne="https://jerrychiu0863.github.io/multi-layer-banner/"
+          projectTwo={Red}
+          titleTwo="Red"
+          urlTwo="https://jerrychiu0863.github.io/red/"
         />
       </div>
     </div>
